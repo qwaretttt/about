f77cb4b2642511e2f1b1db3d5b350dcfb4aa4fb9
qwaretttt <analevus07@gmail.com>
2024-10-26 09:25:07 +0000

    Comment

--- js/index.aboutus-clock.js
+++ js/index.aboutus-clock.js
@@ -1,3 +1,7 @@
 const clockContainer = document.querySelector(".aboutus__clock");
 
+function updateClock() {
 clockContainer.innerText = new Date().toLocaleTimeString('uk');
+}
+
+setInterval(updateClock, 1000);

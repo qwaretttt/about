54a0d61e2aaa34b21e65e28824ddc99e10d43dc8
qwaretttt <analevus07@gmail.com>
2024-10-27 15:33:45 +0000

    Comment

--- js/index.aboutus.js
+++ js/index.aboutus.js
@@ -1,41 +1,3 @@
-const aboutus = [
-    {
-        id: '1',
-        title: '\\ ABOUT US \\',
-        subtitle: 'Illuminate Your Space, Illuminate Your Life With Us',
-        description: 'Get a lighting solution that suits your needs.'
-    },
-    {
-    id: '2',
-    subtitle: 'Place the Right Accents with Modern Lights by Prizma Studio',
-    text: 'We closely cooperate with our clients to fully understand their needs and goals during the early stages of the project. We provide accurate lighting analysis, detailed specifications, schedules for A/V integration, detailed fixture design, and more.',
-    texr: 'Our true passion is the art of integrating light within architecture. We have completed over 2000 residential projects and built complex lighting system designs nearly in every interior and exterior imaginable.'
-    }
-]
-
-function renderAboutus(aboutus) {
-    let aboutusHtml = '';
-    for (const about of aboutus) {
-        aboutusHtml += `
-    <div class="aboutus">
-      <div class="aboutus__content aboutus__content--left">
-        <h2 class="aboutus__title">${about.title}</h2>
-        <h3 class="aboutus__subtitle">${about.subtitle}</h3>
-        <p class="aboutus__description">${about.description}</p>
-      </div>
-      <div class="aboutus__content aboutus__content--right">
-        <h3 class="aboutus__subtitle">${about.subtitle}</h3>
-        <p class="aboutus__text">${about.text}</p>
-        <p class="aboutus__text">${about.text}</p>
-      </div>
-    </div>
-     `;
-}
-   document.querySelector('.aboutus__content').innerText = aboutusHtml;
-}
-
-renderAboutus(aboutus);
-
 const statistics = [
 {
 id: '1',
@@ -62,30 +24,19 @@ description: 'Lamps installed in 2023'
 ]
 
 function renderAboutus(statistics) {
-  let aboutusHtml = '';
+  let statisticsHtml = '';
   for (const statistic of statistics) {
-      aboutusHtml += `
+     statisticsHtml += `
  <div class="statistics__item">
         <p class="statistics__number">${statistic.number}</p>
-        <p class="statistics__sign">${statistic.sign}</p>
-        <p class="statistics__description">${statistic.description}</p>
-      </div>
-      <div class="statistics__item">
-        <p class="statistics__number">${statistic.number}</p>
-        <p class="statistics__description">${statistic.description}</p>
-      </div>
-      <div class="statistics__item">
-        <p class="statistics__number">${statistic.number}</p>
-        <p class="statistics__sign">${statistic.sign}</p>
-        <p class="statistics__description">${statistic.description}</p>
-      </div>
-      <div class="statistics__item">
-        <p class="statistics__number">${statistic.number}</p>
+          ${statistic.sign ? `<p class="statistics__sign">${statistic.sign}</p>` : ''}
         <p class="statistics__description">${statistic.description}</p>
       </div>
         `;
 }
-   document.querySelector('.statisctics').innerText = statisticsHtml;
+   document.querySelector('.statistics__list').innerHTML = statisticsHtml;
 }
 
 renderAboutus(statistics);
+
+const = 
